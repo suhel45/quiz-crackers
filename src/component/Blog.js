eb0347d17b21789ca712540bfd2f1f8d6bf8db02
suhel45 <suhel.ict45@gmail.com>
2022-10-12 21:59:31 +0600

    rechart added

--- src/component/Blog.js
+++ src/component/Blog.js
@@ -4,7 +4,7 @@ const Blog = () => {
     return (
       <div>
         <div>
-        <p>Perpas of React Router?</p>
+        <p className='text-3xl'>Perpas of React Router?</p>
         <p>React Router is a standard library for routing in React. It enables the navigation among 
         views of various components in a React Application, allows changing the browser URL, and 
         keeps the UI in sync with the URL.</p>
--- src/component/Statistics.js
+++ src/component/Statistics.js
@@ -1,20 +1,37 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
 import StatisticsChart from './StatisticsChart';
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+
 
 const Statistics = () => {
     const loadData = useLoaderData().data;
     let total = [];
-    loadData.map(datas =>total.push(datas.total));
+    loadData.map(datas =>total.push({name:datas.name,total:datas.total}));
     console.log(total)
-    const data = [{name: 'Page A', uv: 400, pv: 2400, amt: 2400}];
+    const data = [{name: 'Page A', uv: total, pv: 2400, amt: 2400}];
 
     return (
         <div>
-            <LineChart width={400} height={300} data={data}></LineChart>
-            <Line type="monotone" dataKey="uv" stroke="#8884d8" fill="#8884d8" />
-    </div>
+        <BarChart
+        width={500}
+        height={400}
+        data={total}
+        margin={{
+          top: 5,
+          right: 30,
+          left: 20,
+          bottom: 5,
+        }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="name" />
+        <YAxis dataKey="total" />
+        <Tooltip />
+        <Legend />
+        <Bar dataKey="total" fill="#8884d8" />
+      </BarChart>
+    </div> 
     );
 };
 
