29fdbe7e67d5cc2f998b02500fe960225573d9c5
suhel45 <suhel.ict45@gmail.com>
2022-10-12 22:02:51 +0600

    fix

--- src/component/Blog.js
+++ src/component/Blog.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Blog = () => {
     return (
-      <div>
+      <div className='container mx-auto'>
         <div>
         <p className='text-3xl'>Perpas of React Router?</p>
         <p>React Router is a standard library for routing in React. It enables the navigation among 
@@ -10,11 +10,11 @@ const Blog = () => {
         keeps the UI in sync with the URL.</p>
         </div>
         <div>
-          <p>How does context Api work?</p>
+          <p className='text-3xl'>How does context Api work?</p>
           <p>The React Context API is a way for a React app to effectively produce global variables that can be passed around. This is the alternative to "prop drilling" or moving props from grandparent to child to parent, and so on. Context is also touted as an easier, lighter approach to state management using Redux.</p>
         </div>
         <div>
-          <p>What is useRef?</p>
+          <p className='text-3xl'>What is useRef?</p>
           <p>Essentially, useRef is like a “box” that can hold a mutable value in its .current property. You might be familiar with refs primarily as a way to access the DOM. If you pass a ref object to React with , React will set its .current property to the corresponding DOM node whenever that node changes.</p>
         </div>
       </div>
