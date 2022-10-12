c4bb16e4ffc0ee5bb2a06531361bbe6ba1cafc45
suhel45 <suhel.ict45@gmail.com>
2022-10-12 20:15:04 +0600

    question added3

--- src/component/Blog.js
+++ src/component/Blog.js
@@ -13,6 +13,10 @@ const Blog = () => {
           <p>How does context Api work?</p>
           <p>The React Context API is a way for a React app to effectively produce global variables that can be passed around. This is the alternative to "prop drilling" or moving props from grandparent to child to parent, and so on. Context is also touted as an easier, lighter approach to state management using Redux.</p>
         </div>
+        <div>
+          <p>What is useRef?</p>
+          <p>Essentially, useRef is like a “box” that can hold a mutable value in its .current property. You might be familiar with refs primarily as a way to access the DOM. If you pass a ref object to React with <div ref={myRef} /> , React will set its .current property to the corresponding DOM node whenever that node changes.</p>
+        </div>
       </div>
     )
 };
